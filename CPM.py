51d6a99dd5f415376e6679026946574d04f01880
Saurav Dash <102579410+DashSaurav@users.noreply.github.com>
2024-02-21 17:04:57 +0530

    Update CPM.py
    Added cache method.

--- CPM.py
+++ CPM.py
@@ -1,9 +1,15 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+@st.cache_data
+def car_data(url):
+    data = conn.read(spreadsheet=url, ttl=36000)
+    return data
+
 url = "https://docs.google.com/spreadsheets/d/1VYrpcJnaw7kPEtUQxxN-_UR2okw8RmtD5YgygWifJmg/edit?usp=sharing"
 
-conn = st.connection("gsheets", type=GSheetsConnection)
-data = conn.read(spreadsheet=url, ttl=3600)
+# data = conn.read(spreadsheet=url)
 
-st.dataframe(data)
+st.dataframe(car_data(url))
